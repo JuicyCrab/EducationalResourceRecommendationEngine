b9f7de0dd46cf359398af0ebfddde2daf1e89cfb
eyasuSmieja <esmieja@students.kennesaw.edu>
2026-06-16 13:17:10 -0400

    Add inverse document frequency

--- retrieval_systems/bm25/bm25_scratch.py
+++ retrieval_systems/bm25/bm25_scratch.py
@@ -7,6 +7,7 @@
 
 from collections import Counter, defaultdict
 import re 
+from math import log 
 
 def tokenize(text) -> list[str]:
     return re.findall(r'\b[a-z]+\b', text.lower())
@@ -26,11 +27,19 @@ def inverted_index(documents) -> dict[str, int]:
             inverted_index_result[term].append(idx)
             
     return inverted_index_result
-            
-            
+             
 
-def inverse_document_frequency() -> dict[str, int]:
-    pass 
+def inverse_document_frequency(total_documents, inverted_index) -> dict[str, int]:
+    """
+        Describes the significance of term rarity across documents. 
+    """
+    N = total_documents
+    idf = {}
+    for key, value in inverted_index.items():
+        idf[key] = log((N - len(value) + 0.5) / (len(value) + 0.5) + 1)
+    
+    return idf 
+    
 
 def full_score():
     pass 
