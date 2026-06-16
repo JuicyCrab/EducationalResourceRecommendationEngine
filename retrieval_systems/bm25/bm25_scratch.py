43091518b2d5f9ba44a54f93e24589444ba7bb89
eyasuSmieja <esmieja@students.kennesaw.edu>
2026-06-16 13:52:24 -0400

    Add bm25 scoring

--- retrieval_systems/bm25/bm25_scratch.py
+++ retrieval_systems/bm25/bm25_scratch.py
@@ -5,19 +5,20 @@
     for different document sizes. 
 """
 
+import re
 from collections import Counter, defaultdict
-import re 
-from math import log 
+from math import log
 
 def tokenize(text) -> list[str]:
     return re.findall(r'\b[a-z]+\b', text.lower())
 
 def term_frequency(tokens) -> dict[str, int]:
     return Counter(tokens)
-        
+
 def inverted_index(documents) -> dict[str, int]: 
     """
-        Takes in tokenized documents and maps the term to the multiple documents. Loop through the docs and get term frequency. 
+        Takes in tokenized documents and maps the term to the multiple documents. 
+        Loop through the docs and get term frequency. 
         if that term frequency doesn't exist as a key than add new key. tf = term frequency 
     """
     inverted_index_result = defaultdict(list)
@@ -25,27 +26,40 @@ def inverted_index(documents) -> dict[str, int]:
         tf = term_frequency(doc)
         for term in tf:
             inverted_index_result[term].append(idx)
-            
-    return inverted_index_result
-             
 
-def inverse_document_frequency(total_documents, inverted_index) -> dict[str, int]:
+    return inverted_index_result
+
+
+def inverse_document_frequency(total_number_of_docs, inverted_index) -> dict[str, int]:
     """
         Describes the significance of term rarity across documents. 
     """
-    N = total_documents
     idf = {}
     for key, value in inverted_index.items():
-        idf[key] = log((N - len(value) + 0.5) / (len(value) + 0.5) + 1)
-    
-    return idf 
-    
+        idf[key] = log((total_number_of_docs - len(value) + 0.5) / (len(value) + 0.5) + 1)
 
-def full_score():
-    pass 
+    return idf
+
+def bm25_score(query, doc, doc_len, avg_doc_len, idf_values, k1=1.5, b=0.75) -> float:
+    """
+    Function computes the bm25 score by utilizing the terms in the query and calculating 
+    the inverse document frequency. Also, includes experimental constants 
+    k1(term frequency saturation) and b(length normalization). k1 controls how much 
+    repeated occurrences of a term matter and prevents favoring of documents that 
+    greatly mention a term. b is a penalizer for long documents and values resource 
+    length integrity. 
+    """
+    query_tokens = tokenize(query)
+    tf = term_frequency(doc)
+    score = 0
+    for token in query_tokens:
+        if token not in idf_values:
+            continue
+        score += idf_values[token] * (tf[token] * (k1 + 1)) / (tf[token] + k1 * (1 - b + b* (doc_len / avg_doc_len)))
+    return score
 
 def document_ranking():
-    pass 
+    pass
 
 if __name__ == '__main__':
-    pass 
+    pass
